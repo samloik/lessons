5e8df78c322c9cdb9d9a55ee0f09fd53069b5a52
Алексей <samloik@mail.ru>
2021-10-12 16:53:11 +1000

    lesson #2

--- main.py
+++ main.py
@@ -20,6 +20,43 @@ OUT_FILES = [
 ]
 
 
+# тест функции enumerate
+def test():
+    cols = ["A", "B", "C", "D", "E"]
+    txt = [0, 1, 2, 3, 4]
+
+    # Loop over the rows and columns and fill in the values
+    for num in range(5):
+        row = num
+        print( row)
+        for index, col in enumerate(cols):
+            value = txt[index] + num
+            print(col, index, ' = ', value)
+
+
+import shutil
+from zipfile import ZipFile
+
+#print('problem: >>', filename)
+def problem( filename ):
+    tmp_folder = '/tmp/convert_wrong_excel/'
+    os.makedirs(tmp_folder, exist_ok=True)
+
+    # Распаковываем excel как zip в нашу временную папку
+    print('problem: >>', filename)                        # -----
+    with ZipFile( filename ) as excel_container:
+        excel_container.extractall(tmp_folder)
+
+    # Переименовываем файл с неверным названием
+    wrong_file_path = os.path.join(tmp_folder, 'xl', 'SharedStrings.xml')
+    correct_file_path = os.path.join(tmp_folder, 'xl', 'sharedStrings.xml')
+    os.rename(wrong_file_path, correct_file_path)
+
+    # Запаковываем excel обратно в zip и переименовываем в исходный файл
+    shutil.make_archive( filename, 'zip', tmp_folder)
+    os.rename( filename+'.zip', filename )
+
+
 def andrew_task():
     print('IN_FILES\n', IN_FILES)
     print('\nOUT_FILES\n', OUT_FILES)
@@ -35,22 +72,25 @@ def andrew_task():
     print(file_name_out)
 
     #print( DataFrame1)
+    os.chdir( PATH )
+    #print ( PATH)
 
     #DataFrame1 = pd.ExcelFile(file_name1)
 
-    try_load2( file_name1 )
+    try_load2( file_name2 )
 
 def try_load2( f ):
-    file_name2 = f
-    DataFrame1 = pd.read_excel( file_name2 )
-    print(DataFrame1)
-    print( DataFrame1 )
+    file_name = f
+    problem( file_name)
+    #DataFrame1 = pd.read_excel( file_name )
+
+    #print( DataFrame1 )
 
 
 def try_load( f ):
-    file_name2 = f
+    file_name = f
     try:
-         DataFrame1 = pd.read_excel( file_name2 )
+         DataFrame1 = pd.read_excel( file_name )
          print(DataFrame1)
     except KeyError as Error:
         if Error == "There is no item named 'xl/sharedStrings.xml' in the archive":
@@ -73,44 +113,3 @@ andrew_task()
 print('\n\n hello ')
 
 
-
-# тест функции enumerate
-def test():
-    cols = ["A", "B", "C", "D", "E"]
-    txt = [0, 1, 2, 3, 4]
-
-    # Loop over the rows and columns and fill in the values
-    for num in range(5):
-        row = num
-        print( row)
-        for index, col in enumerate(cols):
-            value = txt[index] + num
-            print(col, index, ' = ', value)
-
-
-import shutil
-from zipfile import ZipFile
-
-def problem():
-    # Создаем временную папку
-    tmp_folder = '/tmp/convert_wrong_excel/'
-    os.makedirs(tmp_folder, exist_ok=True)
-
-    # Распаковываем excel как zip в нашу временную папку
-    with ZipFile('yourfile.xlsx') as excel_container:
-        excel_container.extractall(tmp_folder)
-
-    # Переименовываем файл с неверным названием
-    wrong_file_path = os.path.join(tmp_folder, 'xl', 'SharedStrings.xml')
-    correct_file_path = os.path.join(tmp_folder, 'xl', 'sharedStrings.xml')
-    os.rename(wrong_file_path, correct_file_path)
-
-    # Запаковываем excel обратно в zip и переименовываем в исходный файл
-    shutil.make_archive('yourfile', 'zip', tmp_folder)
-    os.rename('yourfile.zip', 'yourfile.xlsx')
-
-
-#test()
-#problem()
-andrew_task()
-print('\n\n hello ')

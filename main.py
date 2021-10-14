c416476d3ab9c1eb7f7bcdf2b0f8012dd1f9d596
Алексей <samloik@mail.ru>
2021-10-14 14:41:48 +1000

    работа:

--- main.py
+++ main.py
@@ -227,11 +227,17 @@ def andrew_task():
 
     print("\ndf2_unique_vals['02_Car'].count():", df2_unique_vals['02_Car'].count())
 
-    df = df2_unique_vals
 
+    df = df2_unique_vals
+    # https://coderoad.ru/35003138/Python-Pandas-%D0%B2%D1%8B%D0%B2%D0%BE%D0%B4-%D1%82%D0%B8%D0%BF%D0%BE%D0%B2-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-%D1%81%D1%82%D0%BE%D0%BB%D0%B1%D1%86%D0%BE%D0%B2
+    print( pd.DataFrame(df.apply(pd.api.types.infer_dtype, axis=0)).reset_index().rename(
+        columns={'index': 'column', 0: 'type'}))
+    """
     # поиск числовых столбцов
     colnames_numerics_only = df.select_dtypes(include=np.number).columns.tolist()
-    print( colnames_numerics_only)
+    print( '\ncolnames_numerics_only:',colnames_numerics_only)
+    """
+    
 
     # добавление столбца сумм по позициям
     df = df2_unique_vals.append({'item_code': 'Total'}, ignore_index=True)

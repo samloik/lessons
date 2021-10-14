063d6f2c6d42502e75253323353d65c20d797606
Алексей <samloik@mail.ru>
2021-10-14 14:41:48 +1000

    работа:

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import xlsxwriter
 import openpyxl
 import shutil  # для модуля problem
 from zipfile import ZipFile  # для модуля problem
+import numpy as np # для поиска числовых столбцов
 
 PATH_TO_THE_FILES = {
     'РАБОТА': 'C:\Andrew files',
@@ -226,9 +227,13 @@ def andrew_task():
 
     print("\ndf2_unique_vals['02_Car'].count():", df2_unique_vals['02_Car'].count())
 
-    # добавление столбца сумм по позициям
+    df = df2_unique_vals
 
-    # https://overcoder.net/q/558395/%D0%BA%D0%B0%D0%BA-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D1%83-%D1%81%D1%83%D0%BC%D0%BC%D1%8B-%D0%B8-%D1%81%D1%82%D0%BE%D0%BB%D0%B1%D0%B5%D1%86-%D1%81%D1%83%D0%BC%D0%BC%D1%8B-%D0%B2-%D0%BF%D0%B0%D0%BD%D0%B4%D0%B0%D1%85
+    # поиск числовых столбцов
+    colnames_numerics_only = df.select_dtypes(include=np.number).columns.tolist()
+    print( colnames_numerics_only)
+
+    # добавление столбца сумм по позициям
     df = df2_unique_vals.append({'item_code': 'Total'}, ignore_index=True)
 
     df.loc['Total','02_Car']= df['02_Car'].sum(axis=0)

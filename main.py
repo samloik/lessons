b142ed74cf37db2b39621c0a2ccdf61139ca7179
Алексей <alexey.loik@mail.ru>
2021-10-14 11:40:02 +1000

    работаем с датафреймами отработка метода поиска уникальных и неуникальных ячеек

--- main.py
+++ main.py
@@ -233,7 +233,11 @@ def andrew_task():
         df2.to_excel(writer, sheet_name='df2')
         df3.to_excel(writer, sheet_name='df3')
 
-
+    """
+    информация по методу изменения ширины столбцов
+    
+    https://coderoad.ru/17326973/%D0%95%D1%81%D1%82%D1%8C-%D0%BB%D0%B8-%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8-%D1%80%D0%B5%D0%B3%D1%83%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%88%D0%B8%D1%80%D0%B8%D0%BD%D1%83-%D1%81%D1%82%D0%BE%D0%BB%D0%B1%D1%86%D0%BE%D0%B2-Excel-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E
+    """
 
 
     """ Запись результатов обработки в excel файл2 для сравнения результатов"""
